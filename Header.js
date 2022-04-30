04d0ad54bd409123b5769a5be56e4237e1b977e5
sbolar0 <98791139+sbolar0@users.noreply.github.com>
2022-04-29 23:39:52 -0400

    Add files via upload

--- Header.js
+++ Header.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import styles from "../Styles/Components/HeaderFooter.module.css";
 import HeaderDropdown from "./HeaderDropdown";
 import HeaderDropdownUser from "./HeaderDropdownUser";
+import { UserContext } from "../Hooks/UserContext";
+import { TickerContext } from "../Hooks/TickerContext";
 
 export default function Header() {
-  //if dropdown is set to true you will see dropdown menu
-  const [dropdown, setDropdown] = useState(true);
+  // if dropdown is set to true you will see dropdown menu
+  const [dropdown, setDropdown] = useState(false);
+  const { loggedIn, setLogIn } = useContext(UserContext);
+  const { tickerType, setTickerType } = useContext(TickerContext);
 
-  //these two onMouse functions toggle the dropdown state so you see it when mouse is over div where dropdwon is located
+  // so you see it when mouse is over div where dropdwon is located
   const onMouseEnter = () => {
     setDropdown(true);
   };
@@ -15,20 +19,19 @@ export default function Header() {
   const onMouseLeave = () => {
     setDropdown(false);
   };
+
+  const onChangeValue = (event) => {
+    setLogIn(true);
+    setTickerType(event.target.value);
+  };
+
   return (
     // <div>
     <div className={styles.headerContainer}>
-      {/* {sessionStorage.access_token && (
-        <div className={styles.centerForm}>
-          <b>Logged In</b>
-        </div>
-      )} */}
-
       {/* THIS IS WHERE WE CHECK TO SEE IF USER IS LOGGED IN */}
-      {sessionStorage.access_token ? (
+      {loggedIn ? (
         <div>
           <div className={styles.left}>
-            {/* <b>Logged In</b> */}
             <div
               className={styles.navitem}
               onMouseEnter={onMouseEnter}
@@ -39,7 +42,19 @@ export default function Header() {
               {dropdown && <HeaderDropdownUser />}
             </div>
           </div>
+          <div className={styles.centerForm}>
+            <input
+              className={styles.inputField}
+              type="text"
+              name="stockSearch"
+              placeholder="search for stock"
+            ></input>
+            <div onChange={onChangeValue} className={styles.centerForm}>
+              <input type="radio" value="Stock" name="tickerType" /> Stock
+              <input type="radio" value="Crypto" name="tickerType" /> Crypto
+            </div>
           </div>
+        </div>
       ) : (
         // NOT LOGGED IN
         <div>
@@ -54,10 +69,9 @@ export default function Header() {
               {dropdown && <HeaderDropdown />}
             </div>
           </div>
-          {/* <div className={styles.centerForm}>Not Logged In</div> */}
         </div>
       )}
-       <div className={styles.centerForm}>
+      <div className={styles.centerForm}>
             <input
               className={styles.inputField}
               type="text"
